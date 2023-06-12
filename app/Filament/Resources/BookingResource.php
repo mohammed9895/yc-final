23b1ab0c0a77a807ec8c453bbbcb9a8f9d9a6624
Mohammed Hamad <toshiba9895@gmail.com>
2023-06-12 17:29:27 +0400

    add ans to booking table

--- app/Filament/Resources/BookingResource.php
+++ app/Filament/Resources/BookingResource.php
@@ -118,6 +118,7 @@ class BookingResource extends Resource
                 Tables\Columns\TextColumn::make('user.email')
                     ->label(__('filament::users.email'))
                     ->searchable(),
+                TextColumn::make('answers'),
                 Tables\Columns\TextColumn::make('reasone')->label(__('reasone'))->searchable(),
                 // Tables\Columns\TextColumn::make('answers')->label(__('answers'))->searchable(),
                 Tables\Columns\TextColumn::make('rejection_message')->label(__('rejection_message')),
