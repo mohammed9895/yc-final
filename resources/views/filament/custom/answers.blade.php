d6727595ffae872cf9f8f66b943e597cdc5dfe5b
Mohammed Hamad <toshiba9895@gmail.com>
2023-07-10 14:27:34 +0400

    fix bug

--- resources/views/filament/custom/answers.blade.php
+++ resources/views/filament/custom/answers.blade.php
@@ -1,17 +1,26 @@
 <div>
     @foreach($record->answers as $answer)
+       @php
+           ray($answer);
+       @endphp
         @foreach($answer as $q => $a)
-            <div>
-                @if(str_contains($a, 'answers'))
-                {{$q}} : <a target="_blank" href="/storage/{{ $a }}" class="text-primary-600">Show File</a>
-                @elseif($a == 1)
-                    {{$q}} : Yes
-                @elseif($a == 0)
-                    {{$q}} : No
-                @else
-                {{$q}} : {{$a}}
-                @endif
-            </div>
+            @if(is_array($a))
+                @foreach($a as $answerss)
+                    {{ $answerss }}
+                @endforeach
+            @else
+                <div>
+                    @if(str_contains($a, 'answers'))
+                        {{$q}} : <a target="_blank" href="/storage/{{ $a }}" class="text-primary-600">Show File</a>
+                    @elseif($a == 1)
+                        {{$q}} : Yes
+                    @elseif($a == 0)
+                        {{$q}} : No
+                    @else
+                        {{$q}} : {{$a}}
+                    @endif
+                </div>
+            @endif
         @endforeach
     @endforeach
 </div>
