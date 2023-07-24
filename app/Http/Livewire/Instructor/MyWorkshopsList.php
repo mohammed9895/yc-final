ef13d9e8b709198f995bbbded1c5a2aae3dbd8c7
Mohammed Hamad <toshiba9895@gmail.com>
2023-07-24 20:27:28 +0400

    show all workshops without status in MyWorkshopsList.php

--- app/Http/Livewire/Instructor/MyWorkshopsList.php
+++ app/Http/Livewire/Instructor/MyWorkshopsList.php
@@ -12,7 +12,7 @@ class MyWorkshopsList extends Component
 
     public function render()
     {
-        $this->workshops = Workshop::all()->where('status', '=', 1)->where('user_id', Auth::id());
+        $this->workshops = Workshop::all()->where('user_id', Auth::id());
         return view('livewire.instructor.my-workshops-list');
     }
 }
