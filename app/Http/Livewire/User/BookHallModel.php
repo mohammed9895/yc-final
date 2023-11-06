67514a106989f0a97773aa2c6def4e09503642df
Mohammed Hamad <toshiba9895@gmail.com>
2023-11-06 12:30:01 +0400

    fix hall booking

--- app/Http/Livewire/User/BookHallModel.php
+++ app/Http/Livewire/User/BookHallModel.php
@@ -2,35 +2,23 @@
 
 namespace App\Http\Livewire\User;
 
-use Closure;
-use Carbon\Carbon;
-
-use Filament\Forms;
-use App\Models\Hall;
-use App\Models\Slot;
 use App\Models\Event;
-use Livewire\Component;
-use App\Models\Workshop;
-use Spatie\CalendarLinks\Link;
-use Forms\Components\TextInput;
+use App\Models\Hall;
 use App\Notifications\SmsMessage;
+use Carbon\Carbon;
+use Filament\Forms;
 use Filament\Forms\Components\Grid;
-use Illuminate\Contracts\View\View;
-use Illuminate\Support\Facades\Log;
-use Forms\Components\MarkdownEditor;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Http;
-use LivewireUI\Modal\ModalComponent;
-use Filament\Forms\Components\Select;
-use Illuminate\Support\Facades\Config;
-use Filament\Notifications\Notification;
-use Filament\Notifications\Actions\Action;
 use Filament\Forms\Concerns\InteractsWithForms;
-use Spatie\Ray\Ray;
+use Filament\Notifications\Notification;
+use Forms\Components\MarkdownEditor;
+use Forms\Components\TextInput;
+use Illuminate\Support\Facades\Auth;
+use LivewireUI\Modal\ModalComponent;
 
 class BookHallModel extends ModalComponent implements Forms\Contracts\HasForms
 {
     use InteractsWithForms;
+
     public $hall;
 
 
@@ -48,7 +36,12 @@ class BookHallModel extends ModalComponent implements Forms\Contracts\HasForms
 
     public $counter = 0;
 
-    public $slotsTimings = ['08:00 AM', '08:30 AM', '09:00 AM', '09:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '01:00 PM', '01:30 PM', '02:00 PM', '02:30 PM', '03:00 PM', '03:30 PM', '04:00 PM', '04:30 PM', '05:00 PM', '05:30 PM', '06:00 PM', '06:30 PM', '07:00 PM', '07:30 PM', '08:00 PM', '08:30 PM', '09:00 PM', '09:30 PM', '10:00 PM'];
+    public $slotsTimings = [
+        '08:00 AM', '08:30 AM', '09:00 AM', '09:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM',
+        '12:30 PM', '01:00 PM', '01:30 PM', '02:00 PM', '02:30 PM', '03:00 PM', '03:30 PM', '04:00 PM', '04:30 PM',
+        '05:00 PM', '05:30 PM', '06:00 PM', '06:30 PM', '07:00 PM', '07:30 PM', '08:00 PM', '08:30 PM', '09:00 PM',
+        '09:30 PM', '10:00 PM'
+    ];
 
     public function mount(Hall $hall)
     {
@@ -67,48 +60,17 @@ class BookHallModel extends ModalComponent implements Forms\Contracts\HasForms
         }
     }
 
-
-    protected function getFormSchema(): array
-    {
-        return [
-            Grid::make()
-                ->schema([
-                    Forms\Components\TextInput::make('title')
-                        ->required()
-                        ->label(__('Title'))
-                        ->maxLength(255),
-                    Forms\Components\TextInput::make('reasone')
-                        ->required()
-                        ->label(__('reasone'))
-                        ->maxLength(255),
-                    Forms\Components\TextInput::make('pax')->required()->numeric()->label(__('pax')),
-                    Forms\Components\DatePicker::make('date')
-                        ->withoutSeconds()
-                        ->label(__('filament::yc.date'))
-                        ->minDate(now()->today())
-                        ->reactive()
-                        ->afterStateUpdated(function (callable $set, $state) {
-                            $this->slots = [];
-                            $set('todayDate', $state);
-                        })
-                        ->weekStartsOnSunday()
-                        ->required(),
-                ])
-        ];
-    }
-
-
     public function create(): void
     {
         $orginal = $this->form->getState();
         $orginal['user_id'] = $this->user_id;
         $orginal['hall_id'] = $this->hall_id;
 
-        if($this->slots != []) {
+        if ($this->slots != []) {
             $startTime = min($this->slots);
             $endTime = max($this->slots);
-            $startDateAndTime = Carbon::parse($orginal['date'] . $startTime);
-            $endDateAndTime = Carbon::parse($orginal['date'] . $endTime);
+            $startDateAndTime = Carbon::parse($orginal['date'].$startTime);
+            $endDateAndTime = Carbon::parse($orginal['date'].$endTime);
 
             $orginal['start'] = $startDateAndTime;
 
@@ -127,7 +89,6 @@ class BookHallModel extends ModalComponent implements Forms\Contracts\HasForms
                 ->whereIn('status', [0, 1])
                 ->where('user_id', auth()->id())
                 ->exists();
-
 
 
             if ($events > 0) {
@@ -168,19 +129,18 @@ class BookHallModel extends ModalComponent implements Forms\Contracts\HasForms
                     $user = auth()->user();
                     if ($user->preferred_language == 'ar') {
                         $sms->to($user->phone)
-                            ->message('تم استلام طلبك حجزك ل ' . $this->hall->name . ' سوف يتم تأكيد حجزك قريبًا')
+                            ->message('تم استلام طلبك حجزك ل '.$this->hall->name.' سوف يتم تأكيد حجزك قريبًا')
                             ->lang($user->preferred_language)
                             ->send();
                     } else {
                         $sms->to($user->phone)
-                            ->message("Your reservation has been received for " . $this->hall->name . ", It will be confirmed soon.")
+                            ->message("Your reservation has been received for ".$this->hall->name.", It will be confirmed soon.")
                             ->lang($user->preferred_language)
                             ->send();
                     }
                 }
             }
-        }
-        else {
+        } else {
             session()->flash('error', __('You have to choose timing!'));
         }
     }
@@ -195,7 +155,8 @@ class BookHallModel extends ModalComponent implements Forms\Contracts\HasForms
             $nextSlot = Carbon::createFromFormat('h:i A', $this->slots[$i + 1]);
             $difference = $currentSlot->diffInMinutes($nextSlot);
 
-            // Check if the difference between two slots is not equal to 30 (or your slot interval)
+            ray("Comparing {$this->slots[$i]} and {$this->slots[$i + 1]}: Difference is {$difference} minutes");
+
             if ($difference != 30) {
                 session()->flash('error', __('The selected slots are not consecutive!'));
                 $this->slots = [];
@@ -214,14 +175,22 @@ class BookHallModel extends ModalComponent implements Forms\Contracts\HasForms
 
             if ($dayOfWeek === Carbon::FRIDAY) {
                 // Assign your modified slots for Friday
-                $this->slotsTimings = ['01:00 PM', '01:30 PM', '02:00 PM', '02:30 PM', '03:00 PM', '03:30 PM', '04:00 PM', '04:30 PM', '05:00 PM', '05:30 PM', '06:00 PM', '06:30 PM', '07:00 PM', '07:30 PM', '08:00 PM', '08:30 PM', '09:00 PM', '09:30 PM', '10:00 PM'];
+                $this->slotsTimings = [
+                    '01:00 PM', '01:30 PM', '02:00 PM', '02:30 PM', '03:00 PM', '03:30 PM', '04:00 PM', '04:30 PM',
+                    '05:00 PM', '05:30 PM', '06:00 PM', '06:30 PM', '07:00 PM', '07:30 PM', '08:00 PM', '08:30 PM',
+                    '09:00 PM', '09:30 PM', '10:00 PM'
+                ];
             } else {
                 // Assign your default slots for other days
-                $this->slotsTimings = ['08:00 AM', '08:30 AM', '09:00 AM', '09:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '01:00 PM', '01:30 PM', '02:00 PM', '02:30 PM', '03:00 PM', '03:30 PM', '04:00 PM', '04:30 PM', '05:00 PM', '05:30 PM', '06:00 PM', '06:30 PM', '07:00 PM', '07:30 PM', '08:00 PM', '08:30 PM', '09:00 PM', '09:30 PM', '10:00 PM'];
+                $this->slotsTimings = [
+                    '08:00 AM', '08:30 AM', '09:00 AM', '09:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM',
+                    '12:00 PM', '12:30 PM', '01:00 PM', '01:30 PM', '02:00 PM', '02:30 PM', '03:00 PM', '03:30 PM',
+                    '04:00 PM', '04:30 PM', '05:00 PM', '05:30 PM', '06:00 PM', '06:30 PM', '07:00 PM', '07:30 PM',
+                    '08:00 PM', '08:30 PM', '09:00 PM', '09:30 PM', '10:00 PM'
+                ];
             }
         }
     }
-
 
     public function render()
     {
@@ -243,5 +212,34 @@ class BookHallModel extends ModalComponent implements Forms\Contracts\HasForms
         $this->timings = $reservedTimings;
 
         return view('livewire.user.book-hall-model');
+    }
+
+    protected function getFormSchema(): array
+    {
+        return [
+            Grid::make()
+                ->schema([
+                    Forms\Components\TextInput::make('title')
+                        ->required()
+                        ->label(__('Title'))
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('reasone')
+                        ->required()
+                        ->label(__('reasone'))
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('pax')->required()->numeric()->label(__('pax')),
+                    Forms\Components\DatePicker::make('date')
+                        ->withoutSeconds()
+                        ->label(__('filament::yc.date'))
+                        ->minDate(now()->today())
+                        ->reactive()
+                        ->afterStateUpdated(function (callable $set, $state) {
+                            $this->slots = [];
+                            $set('todayDate', $state);
+                        })
+                        ->weekStartsOnSunday()
+                        ->required(),
+                ])
+        ];
     }
 }
